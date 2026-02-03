c8e1bd3f1f13a1ca1f15105015a94045b9d36d18
vbaresc657 <vbarraganescudero@gmail.com>
2026-02-03 10:30:22 +0100

    Update: Ejercicios de bash victor manuel

--- ejer6.sh
+++ ejer6.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 clear
-nombres=('victor' 'manuel')
-apellidos=('barragan' 'escudero')
-    if [[ ${nombres[@]} != ${apellidos[@]} ]];then
-        echo "No son iguales"
-        echo ${nombres[@]}
+
+nombres=('victor' 'manuel' 'barragan')
+apellidos=('barragan' 'escudero' 'victor')
+
+resultado=()
+
+for nombre in "${nombres[@]}"; do
+    # Comprobamos si el nombre actual NO está en el array de apellidos
+    if [[ ! " ${apellidos[@]} " =~ " ${nombre} " ]]; then
+        resultado+=("$nombre")
     fi
+done
+
+echo "Nombres del primer array que no están en el segundo:"
+echo "${resultado[@]}"
